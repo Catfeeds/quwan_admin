7f46c38ffb65ceedfd9e0f31f9f82618a10c881b
bobo <273719650@qq.com>
2018-01-19 14:30:41 +0800

    t

---
+++ App/Home/Controller/AdController.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * 版权所有：波波<www.bobolucy.com>
+ * 作    者：波波<273719650@qq.com>
+ * 日    期：2016-01-21
+ * 版    本：1.0.0
+ * 功能说明：前台控制器演示。
+ *
+ **/
+namespace Home\Controller;
+
+
+class AdController extends ComController
+{
+    public function index()
+    {
+        $id = $_GET['adv_id'];
+        $flash = M('adv')->where('adv_status=1 and adv_id=' . $id)->find();
+        if(!$flash || $flash['adv_type']==1){
+            $title = "趣玩";
+            $desc = "请求出错了";
+        }else{
+            $title = $flash['adv_title'];
+            $desc = $flash['adv_content'];
+        }
+        $this->assign("title",$title);
+        $this->assign("desc",$desc);
+        $this->display();
+    }
+}
